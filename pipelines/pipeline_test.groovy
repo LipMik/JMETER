9d0b8dc652c3aacb6a11ee8723f27e87e149c6bc
Mikalai Lipski <lipski.mikalai@gmail.com>
2022-02-10 20:15:12 +0300

    pipeline commit

---
+++ pipelines/pipeline_test.groovy
@@ -0,0 +1,31 @@
+#!/usr/bin/env groovy
+    node {
+
+      stage('Initialise') {
+        /* Checkout the scripts */
+        checkout scm: [
+                $class: 'GitSCM',
+                userRemoteConfigs: [
+                        [
+                                url: "https://github.com/LipMik/JMETER.git",
+                                credentialsId: "LipMik"
+                        ]
+                ],
+                branches: [[name: "main"]]
+        ], poll: false
+      }
+
+      stage('Complete any setup steps') {
+		echo "Complete set-up steps"
+      }
+
+      stage('Execute Performance Tests') {
+        dir("${WORKSPACE}/scripts") {
+			bat 'C:\Users\Lenovo\Downloads\apache-jmeter-5.4.3\bin\jmeter -j jmeter.save.saveservice.output_format=xml -n -t C:\Users\Lenovo\Downloads\apache-jmeter-5.4.3\bin\postgre_tests.jmx -l C:\Users\Lenovo\Downloads\apache-jmeter-5.4.3\bin\report.jtl'
+        }
+      }
+
+      stage('Analyse Results') {
+		echo "Analyse results"
+      }
+    }
